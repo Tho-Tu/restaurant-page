a47f2a6bcb963f70553d77834ea58d6884537ed5
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-08-30 10:03:59 +1000

    Create home page with navbar and headline

--- src/homePage.js
+++ src/homePage.js
@@ -1,7 +1,6 @@
 import "./style.css";
 
 export default function loadHomePage() {
-  console.log("load home page");
   const contentDiv = document.querySelector("#content");
 
   // NAVIGATION BAR
@@ -12,6 +11,7 @@ export default function loadHomePage() {
   for (const link of navBarLinks) {
     const navLink = document.createElement("li");
     navLink.textContent = `${link}`;
+    navLink.setAttribute("id", `${link}Page`);
     hyperLinks.appendChild(navLink);
   }
 
@@ -20,7 +20,17 @@ export default function loadHomePage() {
 
   // HEADLINE
   const headLine = document.createElement("div");
+  headLine.setAttribute("class", "head-line");
+  headLine.textContent = "The Restaurant";
+  contentDiv.appendChild(headLine);
 
   // ATTRIBUTION
-  const attribution = document.createElement("div");
+  const attribution = document.createElement("a");
+  attribution.setAttribute("class", "attribution");
+  attribution.setAttribute(
+    "href",
+    "https://www.freepik.com/free-photo/restaurant-interior_1243339.htm#query=restuarant&position=11&from_view=search&track=sph"
+  );
+  attribution.textContent = "Image by evening_tao on Freepik";
+  contentDiv.appendChild(attribution);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,14 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [
@@ -15,4 +20,7 @@ module.exports = {
       },
     ],
   },
+  //   optimization: {
+  //     runtimeChunk: "single",
+  //   },
 };
